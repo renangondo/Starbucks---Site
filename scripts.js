8b2410304e97f309e584b91de309db2cdab66605
renangondo <renanluizgondo@gmail.com>
2025-12-14 11:38:29 -0300

    Feito toda a estilização

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/*
+    JavaScripit não é JAVA
+    Variaveis (Qualquer Liguagem de programação)
+        - Um pedacinho da memória do computador - separa o que você quer guardar
+        - Armazena valores (Números, textos, booleanos)
+        - Podemos manipular esses valores
+        - Usamos um let ou const para declarar uma variável
+        - console.log() -> Mostra algo no console do navegador
+     
+
+*/
